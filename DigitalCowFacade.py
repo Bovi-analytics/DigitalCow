d8c7a10fddce84142321da9c233a5af2df531d1d
GabevandenHoeven <gcw.vandenhoeven@student.han.nl>
2023-03-30 23:34:55 +0200

    replaced the abstract facade class with the python package

--- DigitalCowFacade.py
+++ DigitalCowFacade.py
@@ -1,64 +1,7 @@
 from DigitalCow import DigitalCow
 from DairyState import State
-from abc import ABC, abstractmethod
+from chain_simulator import AbstractDigitalTwinFacade
 from decimal import Decimal
-from typing import Generic, TypeVar
-
-T = TypeVar("T")
-
-
-class AbstractDigitalTwinFacade(ABC, Generic[T]):
-    """An abstract base class for a digital twin facade.
-
-    A digital twin facade is a class that represents a digital twin and provides a high-level
-    interface for interacting with it. This class defines the basic properties and methods that
-    all digital twin facades should have.
-    """
-
-    def __init__(self, digital_twin: T, states: tuple[str, ...]) -> None:
-        """Initialize a new instance of the AbstractDigitalTwinFacade class.
-
-        :param digital_twin: The digital twin that this facade represents.
-        :type digital_twin: T
-        :param states: The states that the digital twin can be in.
-        :type states: tuple[str, ...]
-        """
-        self._digital_twin = digital_twin
-        self._states = states
-
-    @property
-    def digital_twin(self) -> T:
-        """The digital twin that this facade represents.
-
-        :return: The digital twin that this facade represents.
-        :rtype: T
-        """
-        return self._digital_twin
-
-    @property
-    def states(self) -> tuple[str, ...]:
-        """The states that the digital twin can be in.
-
-        :return: The states that the digital twin can be in.
-        :rtype: tuple[str, ...]
-        """
-        return self._states
-
-    @abstractmethod
-    def probability(self, state_from: str, state_to: str) -> Decimal:
-        """Get the probability of transitioning from one state to another.
-
-        This method should be implemented by concrete subclasses.
-
-        :param state_from: The state that the transition starts from.
-        :type state_from: str
-        :param state_to: The state that the transition ends in.
-        :type state_to: str
-        :return: The probability of transitioning from state_from to state_to.
-        :rtype: Decimal
-        :raises: NotImplementedError
-        """
-        raise NotImplementedError
 
 
 class DigitalCowFacade(AbstractDigitalTwinFacade[DigitalCow]):
