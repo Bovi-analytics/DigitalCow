3984b973a6349e0056c057b01f0045550b80b9ef
Gabe van den Hoeven <55189858+GabevandenHoeven@users.noreply.github.com>
2023-05-15 16:06:01 +0200

    Added plot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # This is a sample Python script.
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+
 import DairyState
 from DigitalCow import DigitalCow, state_probability_generator, convert_final_state_vector
 from DigitalHerd import DigitalHerd
@@ -11,6 +13,7 @@ from chain_simulator.assembly import array_assembler
 import time
 from decimal import Decimal
 import logging
+import matplotlib.pyplot as plt
 
 
 def chain_simulator_test():
@@ -20,7 +23,7 @@ def chain_simulator_test():
                                   days_pregnant=275, age=650, herd=just_another_herd,
                                   state='Pregnant')
     start = time.perf_counter()
-    just_another_cow.generate_total_states(dim_limit=1000, ln_limit=2)
+    just_another_cow.generate_total_states(dim_limit=1000, ln_limit=3)
     end = time.perf_counter()
     print(f"duration for generating states: {end - start}")
     # start = time.perf_counter()
@@ -42,14 +45,19 @@ def chain_simulator_test():
     print(f"duration making TM: {end - start}")
     print(f"validation of the sum of rows being equal to 1: {validate_matrix_sum(tm)}")
     print(f"validation of the probabilities in the matrix being positive: {validate_matrix_positive(tm)}")
-    simulation = state_vector_processor(just_another_cow.initial_state_vector, tm, 140, 7)
+    simulation = state_vector_processor(just_another_cow.initial_state_vector, tm,
+                                        560, 21)
     start = time.perf_counter()
     start1 = start
     for day in simulation:
         end1 = time.perf_counter()
-        print(day)
+        # print(day)
         vector_1d = convert_final_state_vector(day, just_another_cow.total_states, 0)
-        print(vector_1d)
+        # print(vector_1d)
+        xpoints = np.asarray([i for i in vector_1d.keys()])
+        ypoints = np.asarray([i for i in vector_1d.values()])
+        plt.plot(xpoints, ypoints)
+        plt.show()
         print(f"duration of above simulation: {end1 - start1}")
         start1 = time.perf_counter()
 
